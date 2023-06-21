ba5a27a0ffc32870c1a6549dcc2ed2b04cdb71b4
Max Alva Garcia <alva85@gmail.com>
2023-06-20 23:01:39 -0500

    Button ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import {FC} from 'react';
+import Button from '@/app/components/ui/Button';
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
-    return <div>page</div>
+    return <Button>hello world</Button>
 }
 
 export default page;
