2671b652e836d79627b2ad56d4360d98d3de5422
Max Alva Garcia <alva85@gmail.com>
2023-06-21 07:52:19 -0500

    Implementando autenticacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import {db} from '@/lib/db';
+import Button from '@/app/components/ui/Button';
 
 export default async function Home() {
-  return <div className='text-red-500'>Hello World</div>
+  return <Button>Hello</Button>
 }
